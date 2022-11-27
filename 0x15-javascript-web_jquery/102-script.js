26bfbc3db93e2360aa95ee2dc1404c5b8b921378
yobwee-zy <bkingeno07@gmail.com>
2022-11-27 18:18:56 +0000

    Hello everybody!

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,8 @@
+$('document').ready(functio () {
+	const url = 'https://www.fourtonfish.com/hellosalut/?';
+	$('INPUT#btn_translate').click(function () {
+		$.get(url + $.param({ lang:$('INPUT#language_code').val() }), function (data) {
+			$('DIV#hello').html(data.hello);
+		});
+	});
+});
