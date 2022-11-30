1e8ea38be13194cc4b6110da02964b387da31f48
yobwee-zy <bkingeno07@gmail.com>
2022-11-30 08:35:04 +0000

    Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, bode) {
+request(url, function (error, response, body) {
   if (!error) {
     const characters = JSON.parse(body).characters;
-    printCharacters(characers, 0);
+    printCharacters(characters, 0);
   }
 });
 
-funtion printCharacters (characters, index) {
+function printCharacters (characters, index) {
   request(characters[index], function (error, response, body) {
     if (!error) {
       console.log(JSON.parse(body).name);
-      if (index + 1 < characters.lenght) {
-         printCharacters(characters, index + 1);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
       }
     }
   });
