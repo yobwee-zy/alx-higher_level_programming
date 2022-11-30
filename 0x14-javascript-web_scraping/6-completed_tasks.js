523dc3205663d22f83f321aa06956299960ccba7
yobwee-zy <bkingeno07@gmail.com>
2022-11-30 08:23:34 +0000

    Completed tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,25 +1,26 @@
 #!/usr/bin/node
+
 const request = require('request');
 const url = process.argv[2];
 
-requets(url, funtion (err, response, body) {
+request(url, function (err, response, body) {
   if (err) {
     console.log(err);
-  } else if (response.statusCode == 200) {
+  } else if (response.statusCode === 200) {
     const completed = {};
     const tasks = JSON.parse(body);
     for (const i in tasks) {
       const task = tasks[i];
-      if (tasks.completed === true] {
-        if (completed[task.userID] === undefined) {
-	  completed[task.userID] = 1;
-	} else {
-	  completed[task.userID]++;
-	}	
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
       }
     }
-    console.log(completde);
+    console.log(completed);
   } else {
-    console.log('An error occured. status code: ' + response.statusCode);
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });
